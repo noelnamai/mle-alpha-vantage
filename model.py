c5396c8103bf8614316d7cc4ca38f282856423a6
Noel Namai <noelnamai@yahoo.com>
2023-02-23 13:59:43 -0600

    update

--- model.py
+++ model.py
@@ -1,14 +1,9 @@
 import pickle
 import logging
-import datetime
 import numpy as np
 import pandas as pd
 
-from pathlib import Path
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
-
-BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 def train(X_train, y_train):
     model = GradientBoostingClassifier(
@@ -21,10 +16,10 @@ def train(X_train, y_train):
         subsample=0.8,
     )
     model.fit(X_train, y_train)
-    pickle.dump(model, open(Path(BASE_DIR).joinpath("model.pkl"), "wb"))
+    pickle.dump(model, open("model.pkl", "wb"))
 
 def predict(nonfarm_payroll, unemployment_rate, producer_price_index, consumer_price_index, gross_domestic_product, open_price):
-    model = pickle.load(open(Path(BASE_DIR).joinpath("model.pkl"), "rb"))
+    model = pickle.load(open("model.pkl", "rb"))
     X_test = np.array([nonfarm_payroll, unemployment_rate, producer_price_index, consumer_price_index, gross_domestic_product, open_price]).reshape(1, -1)
     y_test = model.predict(X_test)
     return y_test[0]
